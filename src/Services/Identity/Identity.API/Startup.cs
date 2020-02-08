6895f955996d64095002436931753f39574f1b78
Miri <mirvhbv@gmail.com>
2020-02-08 13:35:26 +0400

    Adds identity services to DI

--- src/Services/Identity/Identity.API/Startup.cs
+++ src/Services/Identity/Identity.API/Startup.cs
@@ -10,6 +10,9 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
+using Identity.API.Models;
+using Microsoft.AspNetCore.Identity;
+using Identity.API.Data;
 
 namespace Identity.API
 {
@@ -25,6 +28,11 @@ namespace Identity.API
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            // Identity
+            services.AddIdentity<User, IdentityRole>()
+                .AddEntityFrameworkStores<ApplicationDbContext>()
+                .AddDefaultTokenProviders();
+
             services.AddControllers();
         }
 
