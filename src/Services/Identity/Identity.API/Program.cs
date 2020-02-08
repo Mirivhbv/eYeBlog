f5bbaa5b4c730d386b01103abd37bad85b1ea58c
Miri <miri@vhbv.net>
2020-02-08 13:05:26 +0400

    Fixes type on project name

--- src/Services/Identity/Identity.API/Program.cs
+++ src/Services/Identity/Identity.API/Program.cs
@@ -7,7 +7,7 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
 
-namespace Idetity.API
+namespace Identity.API
 {
     public class Program
     {
--- src/Services/Identity/Identity.API/Startup.cs
+++ src/Services/Identity/Identity.API/Startup.cs
@@ -11,7 +11,7 @@ using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
 
-namespace Idetity.API
+namespace Identity.API
 {
     public class Startup
     {
