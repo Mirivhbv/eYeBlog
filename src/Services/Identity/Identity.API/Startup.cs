01ffaa2ecd321f4ac885834247bde5d3510fb78d
Miri <mirvhbv@gmail.com>
2020-02-08 13:54:35 +0400

    Adds ef core services in startup

--- src/Services/Identity/Identity.API/Startup.cs
+++ src/Services/Identity/Identity.API/Startup.cs
@@ -13,6 +13,8 @@ using Microsoft.Extensions.Logging;
 using Identity.API.Models;
 using Microsoft.AspNetCore.Identity;
 using Identity.API.Data;
+using Microsoft.EntityFrameworkCore;
+using System.Reflection;
 
 namespace Identity.API
 {
@@ -28,6 +30,23 @@ namespace Identity.API
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            
+            // Db
+            // TODO: later refactor it to if-else by using APP_ENV
+            // Until itsn't implemeneted, do comment production version on build.
+            // Production:
+            services.AddDbContext<ApplicationDbContext>(options => 
+                options.UseSqlServer("---connstr---", // TODO: later fetch it from "app secrets"
+                sqlServerOptionsAction: sqlOptions =>
+                {
+                    sqlOptions.MigrationsAssembly(typeof(Startup).GetTypeInfo().Assembly.GetName().Name);
+                    // Configuring Connection Resiliency: https://docs.microsoft.com/en-us/ef/core/miscellaneous/connection-resiliency 
+                    sqlOptions.EnableRetryOnFailure(maxRetryCount: 15, maxRetryDelay: TimeSpan.FromSeconds(30), errorNumbersToAdd: null);
+                }));
+            // Dev:
+            services.AddDbContext<ApplicationDbContext>(options => options.UseInMemoryDatabase("--f1"));
+            
+
             // Identity
             services.AddIdentity<User, IdentityRole>()
                 .AddEntityFrameworkStores<ApplicationDbContext>()
