ee728a64a57bcbf8380c70a4f69481848afcc81b
Miri <mirvhbv@gmail.com>
2020-02-08 13:25:16 +0400

    Corrects namespace of User model

--- src/Services/Identity/Identity.API/Models/User.cs
+++ src/Services/Identity/Identity.API/Models/User.cs
@@ -1,11 +1,14 @@
 using System.ComponentModel.DataAnnotations;
 using Microsoft.AspNetCore.Identity;
 
-public class User : IdentityUser
+namespace Identity.API.Models
 {
-    [Required]
-    public string Firstname { get; set; }
+    public class User : IdentityUser
+    {
+        [Required]
+        public string Firstname { get; set; }
 
-    [Required]
-    public string Lastname { get; set; }
+        [Required]
+        public string Lastname { get; set; }
+    }
 }
